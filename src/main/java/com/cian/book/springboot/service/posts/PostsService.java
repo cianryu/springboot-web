125871f3fe1f0367cdfa1d106b9652d213dec7ef
Cian <78770601+cian-ryu@users.noreply.github.com>
2021-06-09 18:13:11 +0900

    2차 커밋

---
+++ src/main/java/com/cian/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,40 @@
+package com.cian.book.springboot.service.posts;
+
+import com.cian.book.springboot.web.domain.posts.Posts;
+import com.cian.book.springboot.web.domain.posts.PostsRepository;
+import com.cian.book.springboot.web.dto.PostsResponseDto;
+import com.cian.book.springboot.web.dto.PostsSaveRequestDto;
+import com.cian.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
