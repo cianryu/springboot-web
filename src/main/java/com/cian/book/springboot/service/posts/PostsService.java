752ce00263399f04e23401c94744f2e999d94b33
Cian <78770601+cian-ryu@users.noreply.github.com>
2021-06-10 12:59:37 +0900

    oauth 연동

--- src/main/java/com/cian/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/cian/book/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.cian.book.springboot.service.posts;
 
 import com.cian.book.springboot.web.domain.posts.Posts;
 import com.cian.book.springboot.web.domain.posts.PostsRepository;
+import com.cian.book.springboot.web.dto.PostsListResponseDto;
 import com.cian.book.springboot.web.dto.PostsResponseDto;
 import com.cian.book.springboot.web.dto.PostsSaveRequestDto;
 import com.cian.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,5 +39,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
